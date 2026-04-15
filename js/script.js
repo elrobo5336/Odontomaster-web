a311bc2a3c7fdf495576d02ab44c8f3c4cdbcd67
elrobo5336 <fabio.joaquin@hotmail.com>
2026-04-14 22:19:23 -0500

    ahora es responsive el navbar

--- js/script.js
+++ js/script.js
@@ -27,3 +27,11 @@ if (slides.length > 0) {
         showSlide(index);
     }, 4000);
 }
+
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
